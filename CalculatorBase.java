dbdbfcead23787b12175dfe52b5cb1a5b57c0846
Akash-appdeveoper <akashdharun2020@gmail.com>
2025-08-04 18:42:37 +0530

    Initial commit

---
+++ CalculatorBase.java
@@ -0,0 +1,8 @@
+package Task1;
+
+public abstract class CalculatorBase {
+    public abstract double add(double a, double b);
+    public abstract double subtract(double a, double b);
+    public abstract double multiply(double a, double b);
+    public abstract double divide(double a, double b);
+}
